0e29dfaac64208350da770865da12e1dbb21cebb
PeerapatJ <Peerapat,Jardrit@gmail.com>
2022-05-08 00:37:13 +0700

    add function to slider, scene button

--- src/main/java/com/example/demo3/HelloController.java
+++ src/main/java/com/example/demo3/HelloController.java
@@ -4,14 +4,25 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Slider;
 import javafx.scene.shape.Sphere;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class HelloController implements Initializable {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
     @FXML
     private Sphere object;
     private double cordXObject;
@@ -19,11 +30,18 @@ public class HelloController implements Initializable {
 
     @FXML
     private Slider widthController;
-//    private double wDefault = object.getScaleX();
-//    private double hDefault = object.getScaleY();
 
     @FXML
     private Slider hightController;
+
+    @FXML
+    private Button mainButton;
+
+    @FXML
+    private Button logButton;
+
+    @FXML
+    private Button aboutButton;
 
     @FXML
     public void Up(ActionEvent e){
@@ -34,8 +52,6 @@ public class HelloController implements Initializable {
         System.out.println("X:" + cordXObject + ", Y:" + cordYObject);
     }
     public void Right(ActionEvent e){
-        // max y = 210, x = 171
-        // min y = 250, x = 141
         cordXObject = object.getLayoutX();
         if((cordXObject + 5) <= 171 && (cordXObject + 5) >= 141){
             object.setLayoutX(cordXObject += 5);
@@ -57,37 +73,52 @@ public class HelloController implements Initializable {
         System.out.println("X:" + cordXObject + ", Y:" + cordYObject);
     }
 
-    public void WidthSize(ActionEvent e){
-//        widthOfObject = object.getScaleX();
-//        object.setScaleX(widthOfObject += (widthController.getValue() / 10_000) );
-//        System.out.println("widthOfObject:" + widthOfObject);
-//        System.out.println("widthController:" + widthController.getValue());
-    }
-
     public void Reset(ActionEvent e){
         object.setRadius(50);
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        double wDefault = object.getScaleX();
-        double hDefault = object.getScaleY();
-        widthController.valueProperty().addListener(new ChangeListener<Object>() {
-            @Override
-            public void changed(ObservableValue<?> observableValue, Object o, Object t1) {
+        if (object != (null)){
+            double wDefault = object.getScaleX();
+            double hDefault = object.getScaleY();
+            widthController.valueProperty().addListener(new ChangeListener<Object>() {
+                @Override
+                public void changed(ObservableValue<?> observableValue, Object o, Object t1) {
 //                widthOfObject = object.getScaleX();
-                System.out.println("objScaleX:" + widthController.getValue() / 500);
-                System.out.println("w-default: " + wDefault);
-                object.setScaleX(wDefault + (widthController.getValue() / 500));
-            }
-        });
-        hightController.valueProperty().addListener(new ChangeListener<Object>() {
-            @Override
-            public void changed(ObservableValue<? extends Object> observableValue, Object number, Object t1) {
-                System.out.println("objScaleY:" + hightController.getValue() / 500);
-                System.out.println("h-default: " + hDefault);
-                object.setScaleY(hDefault + (hightController.getValue() / 500));
-            }
-        });
+                    System.out.println("objScaleX:" + widthController.getValue() / 500);
+                    System.out.println("w-default: " + wDefault);
+                    object.setScaleX(wDefault + (widthController.getValue() / 500));
+                }
+            });
+            hightController.valueProperty().addListener(new ChangeListener<Object>() {
+                @Override
+                public void changed(ObservableValue<? extends Object> observableValue, Object number, Object t1) {
+                    System.out.println("objScaleY:" + hightController.getValue() / 500);
+                    System.out.println("h-default: " + hDefault);
+                    object.setScaleY(hDefault + (hightController.getValue() / 500));
+                }
+            });
+        }
+    }
+
+    public void goToMain(ActionEvent e) throws IOException {
+        Parent mainFtml = FXMLLoader.load(getClass().getResource("Main.fxml"));
+        Stage mainWindow = (Stage) mainButton.getScene().getWindow();
+        mainWindow.setScene(new Scene(mainFtml));
+        System.out.println("main btn clicked!");
+    }
+    public void goToLog(ActionEvent e) throws IOException {
+        Parent LogFtml = FXMLLoader.load(getClass().getResource("Log.fxml"));
+        Stage mainWindow = (Stage) logButton.getScene().getWindow();
+        mainWindow.setScene(new Scene(LogFtml));
+        System.out.println("log btn clicked!");
+
+    }
+    public void goToAbout(ActionEvent e) throws IOException {
+        Parent AboutFtml = FXMLLoader.load(getClass().getResource("About.fxml"));
+        Stage mainWindow = (Stage) aboutButton.getScene().getWindow();
+        mainWindow.setScene(new Scene(AboutFtml));
+        System.out.println("abt btn clicked!");
     }
 }
